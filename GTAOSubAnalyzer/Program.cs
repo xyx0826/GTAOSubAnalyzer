722e18b565920a7cb7b28d94780146d39da18950
xyx0826 <xyx0826@hotmail.com>
2018-06-20 18:30:27 +0800

    添加项目文件。

---
+++ GTAOSubAnalyzer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GTAOSubAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            Program program = new Program();
+            await program.FetchPosts();
+        }
+
+        async Task FetchPosts()
+        {
+            var afterTime = 1372880565;   // Subreddit creation timestamp
+            var beforeTime = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;    // Current timestamp
+
+            // Expected approx. 296,713 posts so far
+            // EDIT: 137048 posts fetched, setting initial capacity to 150000
+            List<Submission> submissions = new List<Submission>(150000);
+            HttpClient client = new HttpClient();
+            while (beforeTime.CompareTo(afterTime) > 0)
+            {
+                // Pushshift API
+                Console.WriteLine($"Quering 1000 posts before {beforeTime}...");
+                var responseMsg = await client.GetAsync("https://api.pushshift.io/reddit/search/submission/" +
+                    $"?subreddit=gtaonline&sort=desc&sort_type=created_utc&size=1000&after=1372880565&before={beforeTime}");
+                string responseStr = await responseMsg.Content.ReadAsStringAsync();
+                var jo = JObject.Parse(responseStr);
+                var jt = jo.SelectToken("data");
+                var responseList = jt.ToObject<List<Submission>>();
+                if (responseList.Count == 0) break; // Current batch returns none, iteration complete
+                submissions.AddRange(responseList);
+                beforeTime = submissions[submissions.Count - 1].TimeCreated;
+
+                Console.WriteLine($"Writing raw response to {beforeTime}.json...");
+                await File.WriteAllTextAsync($"./{beforeTime}.json", responseStr);
+                Console.WriteLine("Waiting for 1 second before firing next query...");
+                await Task.Delay(1000);
+            }
+            Console.WriteLine($"Fetched a total of {submissions.Count} posts.");
+            Console.WriteLine("Writing serialized JSON to disk...");
+
+            await File.WriteAllTextAsync("./output.json", JsonConvert.SerializeObject(submissions));
+            Console.WriteLine("Exporting complete.");
+            Console.Read();
+        }
+    }
+}
---
+++ GTAOSubAnalyzer/Submission.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+
+namespace GTAOSubAnalyzer
+{
+    /// <summary>
+    /// Object that holds the information of a reddit post.
+    /// </summary>
+    public class Submission
+    {
+        /// <summary>
+        /// The author of the submission.
+        /// </summary>
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        /// <summary>
+        /// The title of the submission.
+        /// </summary>
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The text of the submission (in text mode).
+        /// </summary>
+        [JsonProperty("selftext")]
+        public string Selftext { get; set; }
+
+        /// <summary>
+        /// The creation time, in seconds Unix Timestamp, of the submission.
+        /// </summary>
+        [JsonProperty("created_utc")]
+        public int TimeCreated { get; set; }
+
+        /// <summary>
+        /// The full link to the submission.
+        /// </summary>
+        [JsonProperty("full_link")]
+        public string FullLink { get; set; }
+
+        /// <summary>
+        /// The score of the submission.
+        /// </summary>
+        [JsonProperty("score")]
+        public int Score { get; set; }
+
+        /// <summary>
+        /// The count of comments under the submission.
+        /// </summary>
+        [JsonProperty("num_comments")]
+        public int CommentCount { get; set; }
+
+        /// <summary>
+        /// The count of times the submission being x-posted.
+        /// </summary>
+        [JsonProperty("num_crossposts")]
+        public int CrosspostCount { get; set; }
+
+        /// <summary>
+        /// Whether the submission is NSFW.
+        /// </summary>
+        [JsonProperty("over_18")]
+        public bool IsNSFW { get; set; }
+
+        /// <summary>
+        /// Whether the submission is a spoiler.
+        /// </summary>
+        [JsonProperty("spoiler")]
+        public bool IsSpoiler { get; set; }
+
+        /// <summary>
+        /// Whether the submission is locked by a moderator.
+        /// </summary>
+        [JsonProperty("locked")]
+        public bool IsLocked { get; set; }
+
+        /// <summary>
+        /// The flair of the submission.
+        /// </summary>
+        [JsonProperty("link_flair_text")]
+        public string LinkFlairText { get; set; }
+
+        /// <summary>
+        /// The count of subreddit subscribers by the time the submission is created.
+        /// </summary>
+        [JsonProperty("subreddit_subscribers")]
+        public int SubCount { get; set; }
+    }
+
+}
